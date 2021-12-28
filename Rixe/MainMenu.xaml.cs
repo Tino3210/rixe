4ac0f1e746e7aae9499aa230ba9930666e0da405
petitBiscuits <loic.frossard01@gmail.com>
2021-12-28 17:26:40 +0100

    fix MainMenu inverse btn

--- Rixe/MainMenu.xaml.cs
+++ Rixe/MainMenu.xaml.cs
@@ -26,7 +26,7 @@ namespace Rixe
         private void btn1_Click(object sender, RoutedEventArgs e)
         {
             Trace.WriteLine("Client");
-            MyNetwork.GetInstance("Server");
+            MyNetwork.GetInstance("Client");
             Close();
             new Game().Show();
         }
@@ -34,7 +34,7 @@ namespace Rixe
         private void btn2_Click(object sender, RoutedEventArgs e)
         {
             Trace.WriteLine("Server");
-            MyNetwork.GetInstance("Client");
+            MyNetwork.GetInstance("Server");
             Close();
             new Game().Show();
         }
