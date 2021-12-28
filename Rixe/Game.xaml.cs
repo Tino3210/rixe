162c7e72c0d132f5c2603d4fd15089962ace653a
Tino3210 <izzovalentino0@gmail.com>
2021-12-28 15:20:03 +0100

    refactor: game

--- Rixe/Game.xaml.cs
+++ Rixe/Game.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -21,7 +22,9 @@ namespace Rixe
 
         private DispatcherTimer gameTimer = new DispatcherTimer();
         private List<Rectangle> itemRemover = new List<Rectangle>();
-        private bool moveLeft, moveRight, moveUp, moveDown;        
+        private List<Rectangle> hearts = new List<Rectangle>();
+        private bool moveLeft, moveRight, moveUp, moveDown, canShoot;
+        private Timer cooldownProjectile;
 
         public MainWindow()
         {
@@ -34,13 +37,24 @@ namespace Rixe
             MyCanvas.Focus();
 
             ImageBrush bg = new ImageBrush();
-            bg.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/purple.png"));
-            bg.TileMode = TileMode.Tile;
-            bg.Viewport = new Rect(0,0,.15,.15);
-            bg.ViewboxUnits = BrushMappingMode.RelativeToBoundingBox;
+            bg.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/background.png"));            
             MyCanvas.Background = bg;
 
             player = new Player(MyPlayer);
+
+            hearts.Add(MyHeart1);
+            hearts.Add(MyHeart2);
+            hearts.Add(MyHeart3);
+
+            cooldownProjectile = new Timer(Settings.COOLDOWN_MS);
+            cooldownProjectile.Elapsed += CoolDownEvent;
+
+            canShoot = true;
+        }
+
+        private void CoolDownEvent(Object source, ElapsedEventArgs e)
+        {
+            canShoot = true;
         }
 
         private void GameLoop(object sender, EventArgs e)
@@ -68,25 +82,37 @@ namespace Rixe
 
             foreach (var x in MyCanvas.Children.OfType<Rectangle>())
             {
+                player.PlayerHitBox = new Rect(Canvas.GetLeft(MyPlayer), Canvas.GetTop(MyPlayer), MyPlayer.Width, MyPlayer.Height);
                 if (x is Rectangle && (string)x.Tag == "myProjectile")
                 {
-                    Canvas.SetTop(x, Canvas.GetTop(x) - 20);
-
-                    Rect bulletHitBox = new Rect(Canvas.GetLeft(x), Canvas.GetTop(x), x.Width, x.Height);
+                    Canvas.SetTop(x, Canvas.GetTop(x) - Settings.PROJECTILE_SPEED);                    
 
                     if (Canvas.GetBottom(x) < 0)
                     {
-
                         itemRemover.Add(x);
                     }
                 }
                 if (x is Rectangle && (string)x.Tag == "Projectile")
                 {
-                    Canvas.SetTop(x, Canvas.GetTop(x) + 20);
+                    Canvas.SetTop(x, Canvas.GetTop(x) + Settings.PROJECTILE_SPEED);
 
-                    Rect bulletHitBox = new Rect(Canvas.GetLeft(x), Canvas.GetTop(x), x.Width, x.Height);
-
-                    if (Canvas.GetBottom(x) < Application.Current.MainWindow.Height)
+                    Rect projectilleHitBox = new Rect(Canvas.GetLeft(x), Canvas.GetTop(x), x.Width, x.Height);
+                    
+                    if (player.PlayerHitBox.IntersectsWith(projectilleHitBox))
+                    {                   
+                        itemRemover.Add(x);
+                        player.Health -= 1;                        
+                        if(player.Health < 0)
+                        {
+                            Console.WriteLine("Fin du Game");
+                            player.Health = 3;
+                            hearts[0].Visibility = Visibility.Visible;
+                            hearts[1].Visibility = Visibility.Visible;
+                            hearts[2].Visibility = Visibility.Visible;
+                        }
+                        hearts[player.Health].Visibility = Visibility.Hidden;
+                    }                    
+                    if ((Canvas.GetTop(x) + x.Height) > Application.Current.MainWindow.Height)
                     {
                         itemRemover.Add(x);
                     }
@@ -97,6 +123,7 @@ namespace Rixe
             {
                 MyCanvas.Children.Remove(i);
             }
+            Console.WriteLine(cooldownProjectile.Enabled);            
         }
 
         private void OnKeyDown(object sender, KeyEventArgs e)
@@ -138,10 +165,23 @@ namespace Rixe
                 moveDown = false;
             }
 
-            if (e.Key == Key.Space)
+            if (e.Key == Key.Space && canShoot)
             {
-                Projectile newProjectile = new Projectile("myProjectile", MyPlayer);                
+                Projectile newProjectile = new Projectile("myProjectile", MyPlayer);
+                Rectangle rect = new Rectangle
+                {
+                    Tag = "Projectile",
+                    Height = Settings.PROJECTILE_HEIGHT,
+                    Width = Settings.PROJECTILE_WIDHT,
+                    Fill = Brushes.Yellow,
+                    Stroke = Brushes.Wheat
+                };
+                Canvas.SetLeft(rect, 50);
+                Canvas.SetTop(rect, 0);
+                MyCanvas.Children.Add(rect);
                 MyCanvas.Children.Add(newProjectile.MyProjectile);
+                cooldownProjectile.Start();
+                canShoot = false;
             }
         }
     }
--- Rixe/Settings.cs
+++ Rixe/Settings.cs
@@ -8,9 +8,10 @@ namespace Rixe
 {
     class Settings
     {
-        public static int PLAYER_SPEED = 10;
-        public static int PROJECTILE_SPEED = 20;
-        public static int PROJECTILE_HEIGHT = 20;
-        public static int PROJECTILE_WIDHT = 5;        
+        public static int PLAYER_SPEED = 5;
+        public static int PROJECTILE_SPEED = 10;
+        public static int PROJECTILE_HEIGHT = 32;
+        public static int PROJECTILE_WIDHT = 32;
+        public static int COOLDOWN_MS = 1500;
     }
 }
